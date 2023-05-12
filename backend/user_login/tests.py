5d8840afc0484022089448e2d8f661f7649a453f
Gaurav Joshi <gj451026@gmail.com>
2023-05-12 23:38:20 +0530

    register user added

--- backend/user_login/tests.py
+++ backend/user_login/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RegisterViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('register/')
+        self.valid_payload = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'D@nnyp0laaaa',
+            'first_name': 'Test'
+        }
+        # self.invalid_payload = {
+        #     'username': '',
+        #     'email': 'testuser@example.com',
+        #     'password': 'testpassword',
+        #     'first_name': 'Test'
+        # }
+
+    def test_register_user_with_valid_payload(self):
+        response = self.client.post(self.url, data=self.valid_payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+
+    # def test_register_user_with_invalid_payload(self):
+    #     response = self.client.post(self.url, data=self.invalid_payload)
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #     self.assertEqual(User.objects.count(), 0)
--- backend/user_login/views.py
+++ backend/user_login/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import permissions
-
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from rest_framework import status
 # Create your views here.
 
 
@@ -13,8 +12,44 @@ class RegisterView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, format=None):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Extract user information from the request data
+        username = request.data.get('username')
+        email = request.data.get('email')
+        first_name = request.data.get('first_name')
+        password = request.data.get('password')
+
+        try:
+
+            # Check if all required information is present
+            if username and email and first_name and password:
+
+                # Check if a user with the same email address already exists
+                user_exists = User.objects.filter(email=email).first()
+
+                if user_exists:
+
+                    # If a user with this email already exists, return an error message
+                    return Response({"error": "A user with this credential already exists"}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+
+                    # If the user does not already exist, create a new user and generate access and refresh tokens
+                    user = User.objects.create_user(
+                        username, email=email, first_name=first_name, password=password)
+                    user.name = username
+                    try:
+                        user.save()
+                    except IntegrityError as e:
+
+                        # If an IntegrityError is raised, handle it by printing an error message
+                        print(f"Error saving user: {e}")
+                    else:
+                        # If the save operation completes successfully, refresh the user's information
+                        user.refresh_from_db()
+                        # Generate refresh and access tokens for the new user
+                        refresh = RefreshToken.for_user(user)
+                        return Response({'refresh': str(refresh), 'access': str(refresh.access_token)})
+
+        except ValueError:
+            # If a ValueError is raised, return an error response with a 401 status code
+            return Response(status=401)
