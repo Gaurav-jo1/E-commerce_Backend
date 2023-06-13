2065a55beda67635d95dae7aafca8f08c205295c
Gaurav Joshi <gj451026@gmail.com>
2023-06-13 19:13:27 +0530

    Loginpage added

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,27 +1,58 @@
-import React from 'react'
-import "../styles/HomePage.scss"
-import shoppy_logo from "../assets/logo2.png"
-import {IoSearchSharp} from "react-icons/io5"
+import React, { useState } from "react";
+import "../styles/HomePage.scss";
+import shoppy_logo from "../assets/logo2.png";
+import { IoSearchSharp } from "react-icons/io5";
+import {RxCross2} from "react-icons/rx";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
 // import { Link } from 'react-router-dom'
 
-const HomePage:React.FC = () => {
+const HomePage: React.FC = () => {
+  const [loginOpen, setloginOpen] = useState<boolean>(false);
+  const [signupOpen, setsignupOpen] = useState<boolean>(false);
+
   return (
-    <div className='Homepage_div'>
-        <nav>
-          <ul>
-            <li><img src={shoppy_logo} alt="shoppy logo" height={"30px"} /></li>
-            <li>
-              <input type="text" placeholder='Search...'/>
-              <p> <IoSearchSharp /> </p>
-            </li>
-            <li>
-              <button>Log in</button>
-              <button>Sign up</button>
-            </li>
-          </ul>
-        </nav>
+    <div className="Homepage_div">
+      <div className="Homepage_login">
+        {loginOpen && 
+        <div className="backdrop">
+          <dialog className="Homepage_login-dialog" open>
+            <p className="Homepage_login-close"><RxCross2 onClick={() => setloginOpen(false)}/></p>
+            <LoginPage />
+          </dialog>
+        </div>
+        }
+      </div> 
+      <div className="Homepage_signup">
+        {signupOpen && 
+        <div className="backdrop">
+          <dialog className="Homepage_login-dialog" open>
+            <SignupPage />
+            <button onClick={() => setsignupOpen(false)}>close</button>
+          </dialog>
+        </div>
+        }
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <img src={shoppy_logo} alt="shoppy logo" height={"30px"} />
+          </li>
+          <li>
+            <input type="text" placeholder="Search..." />
+            <p>
+              {" "}
+              <IoSearchSharp />{" "}
+            </p>
+          </li>
+          <li>
+            <button onClick={() => setloginOpen(true)}>Log in</button>
+            <button onClick={() => setsignupOpen(true)}>Sign up</button>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { GoogleLogin } from "@react-oauth/google";
-import axios from "axios";
 
+import axios from "axios";
+import art from "../assets/login_art.jpeg";
+import "../styles/LoginPage.scss";
 
 function handleGoogleLogin(idToken?: string) {
-  axios.post("http://127.0.0.1:8000/google_login/google/", {
+  axios
+    .post("http://127.0.0.1:8000/google_login/google/", {
       id_token: idToken,
     })
     .then(function (response) {
@@ -18,18 +20,48 @@ function handleGoogleLogin(idToken?: string) {
 
 const LoginPage: React.FC = () => {
   return (
-    <div>
-      <div>
-        <GoogleLogin onSuccess={(credentialResponse) => {
-            handleGoogleLogin(credentialResponse.credential);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }} useOneTap
-        />
+    <div className="Loginpage">
+      <div className="Loginpage_login-form">
+        <div className="Loginpage_login-text">
+          <h4>Welcome back to Shoppy!</h4>
+          <h4>Please sign in</h4>
+
+          <div className="Loginpage_signup-link">
+            <p>New to Shoppy? &nbsp; </p>
+            <p>Create an account</p>
+          </div>
+        </div>
+        <div className="Loginpage_google-div">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              handleGoogleLogin(credentialResponse.credential);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }} type="standard" text="continue_with" logo_alignment="left" width="260px" auto_select={false}
+          />
+        </div>
+        <div className="Loginpage_login_form-divider">
+          <p>or</p>
+        </div>
+        <div className="Loginpage_login-inputs">
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <div className="Loginpage_login_forgot-password">
+              <p>Forgot your password?</p>
+            </div>
+            <button>Sign in</button>
+          </form>
+        </div>
       </div>
-      <h1>LoginPage</h1>
-      <Link to="/">Home Page</Link>
+
+      <div className="Loginpage_bg_img">
+        <div className="Loginpage_bg_img-logo"></div>
+        <div className="Loginpage_bg_img-art">
+          <img src={art} alt="art" />
+        </div>
+      </div>
     </div>
   );
 };
