353333ab689cd45696f06a40488016ae154b09bc
Gaurav Joshi <gj451026@gmail.com>
2023-06-26 10:48:33 +0530

    About Section added

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -7,22 +7,27 @@ import MainBG from "../assets/1.webp";
 import "../styles/MainPage.scss";
 
 // Media
+// 1. First Component
 import MainImage1 from "../assets/11.webp";
 import MainImage2 from "../assets/41.webp";
 import MainImage3 from "../assets/21.webp";
 import MainImage4 from "../assets/31.webp";
-
+// 2. Summer text Banner Component
 import HoverImage1 from "../assets/12.webp";
 import HoverImage2 from "../assets/42.webp";
 import HoverImage3 from "../assets/22.webp";
 import HoverImage4 from "../assets/32.webp";
-
+// 3. Shop Options Component
 import ImageCard1 from "../assets/card1.webp";
 import ImageCard2 from "../assets/card2.webp";
-
+// 4. Shoe Banner Component
 import ShoeBanner from "../assets/shoe_banner.png";
-
+// 5. Card Options Component
 import ShoppyLogo from "../assets/android-chrome-512x512.png";
+// 6. Men's, Women and Kids Section Heading
+import Mens from "../assets/men.webp";
+import Women from "../assets/women.webp";
+import Kids from "../assets/kid.webp";
 
 // Interface
 interface ShopImages {
@@ -40,6 +45,13 @@ interface CardImages {
     property2: string;
     property3: string;
     property4: string;
+  };
+}
+
+interface SectionImages {
+  [section: string]: {
+    property1: string;
+    property2: string;
   };
 }
 
@@ -85,6 +97,21 @@ const myCards: CardImages = {
   },
 };
 
+const mySections: SectionImages = {
+  mens: {
+    property1: Mens,
+    property2: "Men's",
+  },
+  Women: {
+    property1: Women,
+    property2: "Women's",
+  },
+  Kid: {
+    property1: Kids,
+    property2: "Kid's",
+  },
+};
+
 const MainPage: React.FC = () => {
   const [isHovered, setIsHovered] = useState<string | null>(null);
 
@@ -107,7 +134,7 @@ const MainPage: React.FC = () => {
           <span>SHOP&nbsp;ALL&nbsp;NEW</span>
         </div>
       </div>
-      
+
       {/* 2. Summer text Banner Component */}
       <div className="Maincontent_container-summer_text">
         <span>Summer Essentials</span>
@@ -145,9 +172,21 @@ const MainPage: React.FC = () => {
         ))}
       </div>
 
-      {/* 4. Card Options Component */}
+      {/* 4. Shoe Banner Component */}
+      <div className="Maincontent_container_shoes-banner">
+        <div className="Shoe_banner-logo_text-container">
+          <img src={ShoppyLogo} alt="Shoppy Logo" />
+          <h1>
+            New <br /> Arrivals
+          </h1>
+        </div>
+        <img src={ShoeBanner} alt="Shoe Banner" />
+        <button>Shop Now</button>
+      </div>
+
+      {/* 5. Card Options Component */}
       <div className="Maincontent_container_cards-options">
-      {Object.entries(myCards).map(([card, properties]) => (
+        {Object.entries(myCards).map(([card, properties]) => (
           <div key={card} className="Maincontent_shop_card-container">
             <img src={properties.property1} alt={card} />
             <div className="Maincontent_shop_card-texts">
@@ -159,14 +198,41 @@ const MainPage: React.FC = () => {
         ))}
       </div>
 
-      {/* 5. Shoe Banner Component */}
-      <div className="Maincontent_container_shoes-banner">
-        <div className="Shoe_banner-logo_text-container">
-          <img src={ShoppyLogo} alt="Shoppy Logo" />
-          <h1>New <br /> Arrivals</h1>
+      {/* 6. Men's, Women and Kids Section Heading*/}
+      <div className="Maincontent_container_section-heading">
+        <p>Explore</p>
+      </div>
+
+      {/* 6. Men's, Women and Kids Section */}
+      <div className="Maincontent_container-gender_sections">
+        {Object.entries(mySections).map(([section, properties]) => (
+          <div key={section} className="Maincontent_container-sections">
+            <img src={properties.property1} alt="" />
+            <button>{properties.property2}</button>
+          </div>
+        ))}
+      </div>
+
+      {/* 7. About Page */}
+      <div className="Maincontent_container-about_section">
+        <h1>
+          MADE FOR THE MODERN <br /> SHOPPING
+        </h1>
+
+        <div className="Maincontent_container-about_text">
+          <p>
+            Welcome to Shoppy, where dreams become fashion reality! We are the
+            trendsetters, the style mavens, and the shopping destination that
+            fuels your passion for all things fabulous. Dive into our carefully
+            curated collection of cutting-edge fashion, statement accessories,
+            and irresistible homeware. From runway-inspired looks to everyday
+            essentials, Shoppy empowers you to express your unique style
+            effortlessly. Step into a world of endless possibilities and let
+            your inner fashionista shine bright with Shoppy!
+          </p>
+
+          <button>ABOUT US</button>
         </div>
-        <img src={ShoeBanner} alt="Shoe Banner" />
-        <button>Shop Now</button>
       </div>
     </div>
   );
