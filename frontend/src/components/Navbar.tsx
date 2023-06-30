bf14f59f51f3abc54a6853a71f88465fdfb0472c
Gaurav Joshi <gj451026@gmail.com>
2023-06-30 16:24:07 +0530

    PostgreSQL added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useContext } from "react";
 
 // Icons
 import { HiMagnifyingGlass } from "react-icons/hi2";
@@ -7,23 +7,48 @@ import { HiMagnifyingGlass } from "react-icons/hi2";
 import { SignComponentProps } from "./CommonInterfaces";
 
 // Styling
-import "../styles/components_styles/Navbar.scss"
+import "../styles/components_styles/Navbar.scss";
+import { AuthContext } from "../context/AuthContext";
 
-const Navbar: React.FC<SignComponentProps> = ({setSignupOpen, setLoginOpen}) => {
-  const [userSearch, setUserSearch] = useState<string>("")
+const Navbar: React.FC<SignComponentProps> = ({
+  setSignupOpen,
+  setLoginOpen,
+}) => {
+  const [userSearch, setUserSearch] = useState<string>("");
+  // const [avatarSrc, setAvatarSrc] = useState();
 
-  console.log(userSearch)
+  const { authTokens } = useContext(AuthContext);
+
+
+  // console.log(userSearch);
   return (
     <nav className="Navbar_container">
-        <div className="Navbar_container-search">
-          <p> <HiMagnifyingGlass /> </p>
-          <input value={userSearch} type="text" placeholder="Search..." onChange={(e) => setUserSearch(e.target.value)} />
+      <div className="Navbar_container-search">
+        <p>
+          {" "}
+          <HiMagnifyingGlass />{" "}
+        </p>
+        <input
+          value={userSearch}
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setUserSearch(e.target.value)}
+        />
+      </div>
+      <div className="Navbar_container-logo">
+        {" "}
+        <p>Shoppy</p>{" "}
+      </div>
+      {authTokens ? (
+        <div className="Navbar_container-profile">
+          {/* <img src={avatarUrl} alt="profile pic" /> */}
         </div>
-        <div className="Navbar_container-logo"> <p>Shoppy</p> </div>
+      ) : (
         <div className="Navbar_container-buttons">
           <p onClick={() => setSignupOpen(true)}>Sign up</p>
           <button onClick={() => setLoginOpen(true)}>Sign in</button>
         </div>
+      )}
     </nav>
   );
 };
