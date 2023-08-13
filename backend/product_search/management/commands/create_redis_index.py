7d4ee2f8aed64f13158bfee4fd1e2623daa35f31
Gaurav Joshi <gj451026@gmail.com>
2023-08-13 10:58:18 +0530

    Redis Index initial Load error resolved

--- backend/product_search/management/commands/create_redis_index.py
+++ backend/product_search/management/commands/create_redis_index.py
@@ -1,5 +1,6 @@
 import redis
 from django.core.management.base import BaseCommand
+from redis.exceptions import ResponseError
 
 
 class Command(BaseCommand):
@@ -9,14 +10,26 @@ class Command(BaseCommand):
         # Establish a connection to the Redis server
         r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
-        # Define the FT.CREATE command
-        command = "FT.CREATE idx:product ON hash PREFIX 1 'product:' SCHEMA name TEXT SORTABLE price NUMERIC SORTABLE image TEXT NOSTEM"
+        # Index name
+        index_name = "idx:product"
 
-        # Execute the command
-        response = r.execute_command(command)
+        try:
+            index_exists = r.execute_command("FT.INFO idx:product")
 
-        # Check if the index was created or already existed
-        if response == "Index already exists":
-            return print("Index already exists")
-        else:
-            return print("RedisCreateIndex: ", response)
+            if index_exists:
+                self.stdout.write(self.style.SUCCESS(f"Index '{index_name}' exists"))
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(f"Index '{index_name}' does not exist")
+                )
+        except ResponseError as e:
+            if "Unknown Index name" in str(e):
+                self.stderr.write(
+                    self.style.ERROR(f"Index '{index_name}' does not exist")
+                )
+                command = "FT.CREATE idx:product ON hash PREFIX 1 'product:' SCHEMA name TEXT SORTABLE price NUMERIC SORTABLE image TEXT NOSTEM"
+                response = r.execute_command(command)
+                return print("RedisCreateIndex: ", response)
+
+            else:
+                self.stderr.write(self.style.ERROR(f"Redis error: {str(e)}"))
