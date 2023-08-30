ff50feb46165e2e12cd41b87845bc7206edc3264
gauravjo1 <gj451026@gmail.com>
2023-08-31 00:57:29 +0530

    NavPage Responsive Added

--- frontend/src/components/NavPage.tsx
+++ frontend/src/components/NavPage.tsx
@@ -4,23 +4,40 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../styles/components_styles/NavPage.scss";
 import { GlobalValue } from "../context/GlobalValue";
-
-import {RxCross1} from "react-icons/rx"
+import { RxCross1 } from "react-icons/rx";
+import { AuthContext } from "../context/AuthContext";
+import {IoIosArrowForward} from "react-icons/io"
 
 const myNavArray: string[] = ["New & Featured", "Men", "Women", "Kids", "Sale"];
 
-const NavLinks: React.FC = () => (
-  <ul>
-    {myNavArray.map((item, index) => (
-      <li key={index}>
-        <Link to={`/${item}`}>{item}</Link>
-      </li>
-    ))}
-  </ul>
-);
 
 const NavPage: React.FC = () => {
-  const { navOptions, setNavOptions } = useContext(GlobalValue);
+  const { navOptions, setNavOptions, setLoginOpen, setSignupOpen } =
+    useContext(GlobalValue);
+  const { authTokens } = useContext(AuthContext);
+
+  const OpenAuth = (isLogin: boolean) => {
+    setNavOptions(false)
+
+    if (isLogin) {
+      setLoginOpen(true)
+    } else {
+      setSignupOpen(true)
+    }
+  }
+
+  const NavLinks: React.FC = () => (
+    <ul>
+      {myNavArray.map((item, index) => (
+        <li key={index} onClick={() =>  setNavOptions(false)}>
+          <Link to={`/${item}`}>
+            {item} <p>{navOptions && (<IoIosArrowForward/>)}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+  
 
   return (
     <>
@@ -29,10 +46,23 @@ const NavPage: React.FC = () => {
       </nav>
       {navOptions && (
         <>
-          <div className="navpage_container_mobile_bg" onClick={() => setNavOptions(false)} />
+          <div
+            className="navpage_container_mobile_bg"
+            onClick={() => setNavOptions(false)}
+          />
           <nav className="navpage_container_mobile">
-            <span onClick={() => setNavOptions(false)}><RxCross1/></span>
+            <span onClick={() => setNavOptions(false)}>
+              <RxCross1 />
+            </span>
             <NavLinks />
+            {!authTokens ? (
+              <div className="navpage_container_mobile_auth">
+                <button onClick={() => OpenAuth(true)}>Sign up</button>
+                <button onClick={() => OpenAuth(false)}>Sign in</button>
+              </div>
+            ) : (
+              <div className="navpage_container_mobile_auth"></div>
+            )}
           </nav>
         </>
       )}
